c07b2dd04f53a9a290bcccd017e70621f2037287
emerson <emerson@emerson-PC>
2013-09-14 11:50:56 -0300

    Criação classe entidade Campus

---
+++ EatSys/src/br/edu/ifnmg/eatsys/DomainModel/Campus.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.eatsys.DomainModel;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author emerson
+ */
+@Entity
+@Table(name="Campus")
+public class Campus implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name="Nome")
+    private String nomeCampus;
+
+    //construtor
+
+    public Campus(Long id, String nomeCampus) {
+        this.id = id;
+        this.nomeCampus = nomeCampus;
+    }
+    
+    //Metodos
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCampus() {
+        return nomeCampus;
+    }
+
+    public void setNomeCampus(String nomeCampus) {
+        this.nomeCampus = nomeCampus;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        hash = 79 * hash + Objects.hashCode(this.nomeCampus);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Campus other = (Campus) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.nomeCampus, other.nomeCampus)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nomeCampus;
+    }
+    
+    
+}
