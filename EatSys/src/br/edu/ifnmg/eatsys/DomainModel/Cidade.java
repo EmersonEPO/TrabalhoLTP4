94057369ffb7be46cf233a5cc4581d975bdc70c2
emerson <emerson@emerson-PC>
2013-09-14 11:50:42 -0300

    Criação classe entidade Cidade

---
+++ EatSys/src/br/edu/ifnmg/eatsys/DomainModel/Cidade.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.eatsys.DomainModel;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author emerson
+ */
+@Entity
+@Table(name="Cidades")
+public class Cidade implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name="Nome")
+    private String nomeCidade;
+    
+    
+    @Column(name="Estado")
+    @ManyToOne
+    private Estado estado;
+    
+    //Construtor
+
+    public Cidade(Long id, String nomeCidade, Estado estado) {
+        this.id = id;
+        this.nomeCidade = nomeCidade;
+        this.estado = estado;
+    }
+    
+    //Metodos
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCidade() {
+        return nomeCidade;
+    }
+
+    public void setNomeCidade(String nomeCidade) {
+        this.nomeCidade = nomeCidade;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cidade)) {
+            return false;
+        }
+        Cidade other = (Cidade) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.edu.ifnmg.eatsys.DomainModel.Cidade[ id=" + id + " ]";
+    }
+    
+}
