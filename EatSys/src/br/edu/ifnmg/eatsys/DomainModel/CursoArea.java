d34009fc1e06b3fc26bb75569b4e9c55227dc92d
emerson <emerson@emerson-PC>
2013-09-14 12:31:37 -0300

    Criação classe entidade CursoArea

---
+++ EatSys/src/br/edu/ifnmg/eatsys/DomainModel/CursoArea.java
@@ -0,0 +1,125 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.eatsys.DomainModel;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author emerson
+ */
+@Entity
+@Table(name="CursosAreas")
+public class CursoArea implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name="Nome")
+    private String nome;
+    
+    @Column(name="Sigla")
+    private String sigla;
+    
+    @Column(name="Status")
+    private boolean status;
+
+    //Construtor
+    
+    public CursoArea() {
+        this.nome = "";
+        this.sigla = "";
+        this.status = true;
+    }
+    
+    public CursoArea(Long id, String nome, String sigla, boolean status) {
+        this.id = id;
+        this.nome = nome;
+        this.sigla = sigla;
+        this.status = status;
+    }
+    //Metodos
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        hash = 97 * hash + Objects.hashCode(this.nome);
+        hash = 97 * hash + Objects.hashCode(this.sigla);
+        hash = 97 * hash + (this.status ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CursoArea other = (CursoArea) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.sigla, other.sigla)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CursoArea{" + "nome=" + nome + ", sigla=" + sigla + '}';
+    }
+    
+    
+}
